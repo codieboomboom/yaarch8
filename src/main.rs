dde364b82f76974854bc6b48649ce719dabd4cc6
Do Anh Tu <anhtu.do1998@gmail.com>
2022-09-05 20:47:44 +0800

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use clap::Parser;
+
+fn main() {
+    let args = Args::parse();
+}
+
+// TODO: Convert to some config file in the future
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Path to ROM file
+    #[clap(short, long)]
+    rom_file_path: String,
+
+    /// Scale factor for display
+    #[clap(short, long, default_value_t = 20)]
+    scale: u32,
+
+    /// Refresh Rate
+    #[clap(short, long, default_value_t = 60)]
+    fps: u32,
+
+    /// CPU frequency
+    #[clap(short, long, default_value_t = 500)]
+    cpu_freq: u32,
+
+    /// Timer frequency
+    #[clap(short, long, default_value_t = 60)]
+    timer_freq: u32,
+}
